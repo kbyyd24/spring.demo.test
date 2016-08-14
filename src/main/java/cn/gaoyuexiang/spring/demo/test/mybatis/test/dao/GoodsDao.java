5612627f9847bab7ab8eb8be129ac308e82608df
gaoyx <melo@gaoyuexiang.cn>
2016-08-14 19:58:07 +0800

    [#1] add GoodsDao and mapper

---
+++ src/main/java/cn/gaoyuexiang/spring/demo/test/mybatis/test/dao/GoodsDao.java
@@ -0,0 +1,11 @@
+package cn.gaoyuexiang.spring.demo.test.mybatis.test.dao;
+
+import cn.gaoyuexiang.spring.demo.test.mybatis.test.model.Goods;
+
+import java.util.List;
+
+public interface GoodsDao {
+	
+	List<Goods> list();
+	
+}
