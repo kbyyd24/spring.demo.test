86740b9a10e38f583b2439f4115adbb9226656af
gaoyx <melo@gaoyuexiang.cn>
2016-08-21 14:24:48 +0800

    [#12] add Component annotation

--- src/main/java/cn/gaoyuexiang/spring/demo/test/aop/car/Ferrari.java
+++ src/main/java/cn/gaoyuexiang/spring/demo/test/aop/car/Ferrari.java
@@ -1,5 +1,8 @@
 package cn.gaoyuexiang.spring.demo.test.aop.car;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Ferrari extends Car{
 	public Ferrari() {
 		this.speed = 340;
